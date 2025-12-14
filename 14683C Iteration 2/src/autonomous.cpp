c5b81f31b415f765fb3c228bb229bb027bf24a07
Wqlffy <86704208+Wqlffy@users.noreply.github.com>
2025-12-14 17:20:23 +0800

    Updated Autonomous

--- 14683C Iteration 2/src/autonomous.cpp
+++ 14683C Iteration 2/src/autonomous.cpp
@@ -1,6 +1,8 @@
 #include "autonomous.hpp"
 #include "main.h"
 #include "lemlib/api.hpp" // IWYU pragma: keep
+#include "pros/distance.hpp"
+#include <sys/_intsup.h>
 
 ASSET(red_left_txt);
 ASSET(red_right_txt);
@@ -13,235 +15,246 @@ extern pros::Motor intake;
 extern pros::Motor indexer;
 extern pros::Motor scoring;
 extern pros::adi::Pneumatics tongue;
-extern pros::adi::Pneumatics aligner;
+extern pros::adi::Pneumatics alignerLeft;
+extern pros::adi::Pneumatics alignerRight;
+extern pros::Distance horizontaldistance;
 
+double readWallIn() {
+    double mm = horizontaldistance.get();
+    if (mm <= 0) return -1;
+    return mm / 25.4;
+}
+
+void driveToWallDistance(double targetIn, int timeoutMs = 800) {
+    const int dt = 10;
+    const double kP = 6.0;
+    const double maxCmd = 45;
+    const double deadband = 0.3;
+
+    int t = 0;
+    while (t < timeoutMs) {
+        double d = readWallIn();
+        if (d < 0) { pros::delay(dt); t += dt; continue; }
+
+        double err = targetIn - d;
+        if (std::fabs(err) < deadband) break;
+
+        double cmd = kP * err;
+        if (cmd >  maxCmd) cmd =  maxCmd;
+        if (cmd < -maxCmd) cmd = -maxCmd;
+
+        chassis.tank(cmd, cmd);
+        pros::delay(dt);
+        t += dt;
+    }
+    chassis.tank(0, 0);
+}
 
 static void wait_ms(int ms) {
     pros::delay(ms);
 }
 
 void auton_red_left() {
-    chassis.setPose(-46.224, 7.195, 0); 
-    chassis.follow(red_left_txt, 12, 8000, true, true);
-
-    //start intake + tongue out
-    chassis.waitUntil(43.952);
-    intake.move_voltage(12000);
-    indexer.move_voltage(-12000);
+    chassis.setPose(0, 0, 0);
+    chassis.moveToPoint(0, 51, 1500, {.maxSpeed = 80});
+    chassis.turnToHeading(-92, 750); 
+    intake.move(-127);
+    indexer.move(-127);
     tongue.set_value(true);
-
-    //wait 1.5 s
-    chassis.waitUntil(59.951);
-    wait_ms(2500);
-
-    // stop intake + tongue in, aligner out
-    chassis.waitUntil(77.973);
-    intake.move_voltage(0);
-    indexer.move_voltage(0);
-    tongue.set_value(false);
-    aligner.set_value(true);
-
-    //scorer + indexer on , wait 1.5 s 
-    chassis.waitUntil(91.973);
-    scoring.move_voltage(12000);
-    indexer.move_voltage(12000);
-    wait_ms(2500);
-
-    //scorer + indexer off intake on
-    chassis.waitUntil(103.483);
-    scoring.move_voltage(0);
-    indexer.move_voltage(0);
-    aligner.set_value(false);
-    intake.move_voltage(12000);
-    indexer.move_voltage(-12000);
-
-    //intake off
-    chassis.waitUntil(123.479);
-    intake.move_voltage(0);
-    indexer.move_voltage(0);
-
-    //scorer positive + indexer negative, wait 1.5 s
-    chassis.waitUntil(134.026);
-    scoring.move_voltage(12000);
-    indexer.move_voltage(-12000);
-    wait_ms(2500);
-    scoring.move_voltage(0);
-    indexer.move_voltage(0);
     
-    chassis.waitUntilDone();
+    pros::delay(800);
 
-    intake.move_voltage(0);
-    indexer.move_voltage(0);
-    scoring.move_voltage(0);
+    chassis.moveToPoint(-12, 51, 1500, {.maxSpeed = 127});
+    pros::delay(1500);
+    chassis.moveToPoint(-1, 51, 1000, {.forwards = false});
+    pros::delay(500);
+    chassis.moveToPoint(-12, 51, 1500, {.maxSpeed = 127});
+    pros::delay(1500);
+
+    chassis.turnToHeading(-90.5, 500);
+    chassis.moveToPoint(0, 51, 2000, {.forwards = false, .maxSpeed = 65});
+    pros::delay(500);
+    tongue.set_value(false);
+    intake.move(0);
+
+    chassis.moveToPoint(22.5, 52, 1500, {.forwards = false, .maxSpeed = 65});
+    scoring.move(127);
+    indexer.move(-127);
+    pros::delay(4000);
+    scoring.move(0);
+    indexer.move(0);
+
+
+    chassis.turnToHeading(-220, 750);
+    // chassis.moveToPoint(13.5, 38, 1500, {.maxSpeed = 65});
+    chassis.moveToPoint(24, 22, 1500, {.maxSpeed = 80});
+    intake.move(-127);
+    indexer.move(-127);
+    pros::delay(2000);
+    intake.move(0);
+    indexer.move(0);
+    
+    chassis.turnToHeading(-398, 750);
+    chassis.moveToPoint(41,5, 2000, {.forwards = false, .maxSpeed = 55});
+    pros::delay(500);
+    scoring.move(127);
+    indexer.move(127);
+    pros::delay(2000);
+    scoring.move(0);
+    indexer.move(0);
 }
 
 void auton_red_right() {
-    chassis.setPose(-46.224, -7.195, 0);
-    chassis.follow(red_right_txt, 12, 8000, true, true);
-
-    //start intake + tongue out
-    chassis.waitUntil(43.952);
-    intake.move_voltage(12000);
-    indexer.move_voltage(-12000);
+    chassis.setPose(0, 0, 0);
+    chassis.moveToPoint(0, 51, 1500, {.maxSpeed = 80});
+    chassis.turnToHeading(90, 750); 
+    intake.move(-127);
+    indexer.move(-127);
     tongue.set_value(true);
-
-    //wait 1.5 s
-    chassis.waitUntil(59.951);
-    wait_ms(2500);
-
-    // stop intake + tongue in, aligner out
-    chassis.waitUntil(77.973);
-    intake.move_voltage(0);
-    indexer.move_voltage(0);
-    tongue.set_value(false);
-    aligner.set_value(true);
-
-    //scorer + indexer on , wait 1.5 s 
-    chassis.waitUntil(91.973);
-    scoring.move_voltage(12000);
-    indexer.move_voltage(12000);
-    wait_ms(2500);
-
-    //scorer + indexer off intake on
-    chassis.waitUntil(103.483);
-    scoring.move_voltage(0);
-    indexer.move_voltage(0);
-    aligner.set_value(false);
-    intake.move_voltage(12000);
-    indexer.move_voltage(-12000);
-
-    //intake off
-    chassis.waitUntil(123.479);
-    intake.move_voltage(0);
-    indexer.move_voltage(0);
-
-    //intake negative + indexer negative, wait 1.5 s
-    chassis.waitUntil(134.026);
-    scoring.move_voltage(-12000);
-    indexer.move_voltage(-12000);
-    intake.move_voltage(-12000);
-    wait_ms(2500);
-    scoring.move_voltage(0);
-    intake.move_voltage(0);
-    indexer.move_voltage(0);
     
-    chassis.waitUntilDone();
+    pros::delay(800);
 
-    intake.move_voltage(0);
-    indexer.move_voltage(0);
-    scoring.move_voltage(0);
+    chassis.moveToPoint(12, 50, 1500, {.maxSpeed = 127});
+    pros::delay(1500);
+    chassis.moveToPoint(1, 50, 1000, {.forwards = false});
+    pros::delay(500);
+    chassis.moveToPoint(12, 50, 1500, {.maxSpeed = 127});
+    pros::delay(1500);
+
+    chassis.turnToHeading(90.5, 500);
+    chassis.moveToPoint(0, 51, 2000, {.forwards = false, .maxSpeed = 65});
+    pros::delay(500);
+    tongue.set_value(false);
+    intake.move(0);
+
+    chassis.moveToPoint(-22.5, 52, 1500, {.forwards = false, .maxSpeed = 65});
+    scoring.move(127);
+    indexer.move(-127);
+    pros::delay(4000);
+    scoring.move(0);
+    indexer.move(0);
+
+
+    chassis.turnToHeading(227, 750);
+    chassis.moveToPoint(-17, 33.6, 1500, {.maxSpeed = 65});
+    chassis.moveToPoint(-26, 22, 1500, {.maxSpeed = 80});
+    intake.move(-127);
+    indexer.move(-127);
+    pros::delay(2000);
+    intake.move(0);
+    indexer.move(0);
+    
+    chassis.moveToPoint(-36,9, 2000, {.maxSpeed = 55});
+    pros::delay(500);
+    scoring.move(-127);
+    indexer.move(-127);
+    intake.move(127);
+    pros::delay(2000);
+    scoring.move(0);
+    indexer.move(0);
+    intake.move(0);
 }
 
+
 void auton_blue_left() {
-    chassis.setPose(46.224, -7.195, 0);
-    chassis.follow(blue_left_txt, 12, 8000, true, true);
-
-    //start intake + tongue out
-    chassis.waitUntil(43.952);
-    intake.move_voltage(12000);
-    indexer.move_voltage(-12000);
+    chassis.setPose(0, 0, 0);
+    chassis.moveToPoint(0, 51, 1500, {.maxSpeed = 80});
+    chassis.turnToHeading(-92, 750); 
+    intake.move(-127);
+    indexer.move(-127);
     tongue.set_value(true);
-
-    //wait 1.5 s
-    chassis.waitUntil(59.951);
-    wait_ms(2500);
-
-    // stop intake + tongue in, aligner out
-    chassis.waitUntil(77.973);
-    intake.move_voltage(0);
-    indexer.move_voltage(0);
-    tongue.set_value(false);
-    aligner.set_value(true);
-
-    //scorer + indexer on , wait 1.5 s 
-    chassis.waitUntil(91.973);
-    scoring.move_voltage(12000);
-    indexer.move_voltage(12000);
-    wait_ms(2500);
-
-    //scorer + indexer off intake on
-    chassis.waitUntil(103.483);
-    scoring.move_voltage(0);
-    indexer.move_voltage(0);
-    aligner.set_value(false);
-    intake.move_voltage(12000);
-    indexer.move_voltage(-12000);
-
-    //intake off
-    chassis.waitUntil(123.479);
-    intake.move_voltage(0);
-    indexer.move_voltage(0);
-
-    //scorer positive + indexer negative, wait 1.5 s
-    chassis.waitUntil(134.026);
-    scoring.move_voltage(12000);
-    indexer.move_voltage(-12000);
-    wait_ms(2500);
-    scoring.move_voltage(0);
-    indexer.move_voltage(0);
     
-    chassis.waitUntilDone();
+    pros::delay(800);
 
-    intake.move_voltage(0);
-    indexer.move_voltage(0);
-    scoring.move_voltage(0);
+    chassis.moveToPoint(-12, 51, 1500, {.maxSpeed = 127});
+    pros::delay(1500);
+    chassis.moveToPoint(-1, 51, 1000, {.forwards = false});
+    pros::delay(500);
+    chassis.moveToPoint(-12, 51, 1500, {.maxSpeed = 127});
+    pros::delay(1500);
+
+    chassis.turnToHeading(-90.5, 500);
+    chassis.moveToPoint(0, 51, 2000, {.forwards = false, .maxSpeed = 65});
+    pros::delay(500);
+    tongue.set_value(false);
+    intake.move(0);
+
+    chassis.moveToPoint(22.5, 52, 1500, {.forwards = false, .maxSpeed = 65});
+    scoring.move(127);
+    indexer.move(-127);
+    pros::delay(4000);
+    scoring.move(0);
+    indexer.move(0);
+
+
+    chassis.turnToHeading(-220, 750);
+    // chassis.moveToPoint(13.5, 38, 1500, {.maxSpeed = 65});
+    chassis.moveToPoint(24, 22, 1500, {.maxSpeed = 80});
+    intake.move(-127);
+    indexer.move(-127);
+    pros::delay(2000);
+    intake.move(0);
+    indexer.move(0);
+    
+    chassis.turnToHeading(-398, 750);
+    chassis.moveToPoint(41,5, 2000, {.forwards = false, .maxSpeed = 55});
+    pros::delay(500);
+    scoring.move(127);
+    indexer.move(127);
+    pros::delay(2000);
+    scoring.move(0);
+    indexer.move(0);
 }
 
 void auton_blue_right() {
-    chassis.setPose(46.224, 7.195, 0);
-    chassis.follow(blue_right_txt, 12, 8000, true, true);
-
-    //start intake + tongue out
-    chassis.waitUntil(43.952);
-    intake.move_voltage(12000);
-    indexer.move_voltage(-12000);
+    chassis.setPose(0, 0, 0);
+    chassis.moveToPoint(0, 51, 1500, {.maxSpeed = 80});
+    chassis.turnToHeading(90, 750); 
+    intake.move(-127);
+    indexer.move(-127);
     tongue.set_value(true);
-
-    //wait 1.5 s
-    chassis.waitUntil(59.951);
-    wait_ms(2500);
-
-    // stop intake + tongue in, aligner out
-    chassis.waitUntil(77.973);
-    intake.move_voltage(0);
-    indexer.move_voltage(0);
-    tongue.set_value(false);
-    aligner.set_value(true);
-
-    //scorer + indexer on , wait 1.5 s 
-    chassis.waitUntil(91.973);
-    scoring.move_voltage(12000);
-    indexer.move_voltage(12000);
-    wait_ms(2500);
-
-    //scorer + indexer off intake on
-    chassis.waitUntil(103.483);
-    scoring.move_voltage(0);
-    indexer.move_voltage(0);
-    aligner.set_value(false);
-    intake.move_voltage(12000);
-    indexer.move_voltage(-12000);
-
-    //intake off
-    chassis.waitUntil(123.479);
-    intake.move_voltage(0);
-    indexer.move_voltage(0);
-
-    //intake negative + indexer negative, wait 1.5 s
-    chassis.waitUntil(134.026);
-    scoring.move_voltage(-12000);
-    indexer.move_voltage(-12000);
-    intake.move_voltage(-12000);
-    wait_ms(2500);
-    scoring.move_voltage(0);
-    intake.move_voltage(0);
-    indexer.move_voltage(0);
     
-    chassis.waitUntilDone();
+    pros::delay(800);
 
-    intake.move_voltage(0);
-    indexer.move_voltage(0);
-    scoring.move_voltage(0);
+    chassis.moveToPoint(12, 50, 1500, {.maxSpeed = 127});
+    pros::delay(1500);
+    chassis.moveToPoint(1, 50, 1000, {.forwards = false});
+    pros::delay(500);
+    chassis.moveToPoint(12, 50, 1500, {.maxSpeed = 127});
+    pros::delay(1500);
+
+    chassis.turnToHeading(90.5, 500);
+    chassis.moveToPoint(0, 51, 2000, {.forwards = false, .maxSpeed = 65});
+    pros::delay(500);
+    tongue.set_value(false);
+    intake.move(0);
+
+    chassis.moveToPoint(-22.5, 52, 1500, {.forwards = false, .maxSpeed = 65});
+    scoring.move(127);
+    indexer.move(-127);
+    pros::delay(4000);
+    scoring.move(0);
+    indexer.move(0);
+
+
+    chassis.turnToHeading(227, 750);
+    chassis.moveToPoint(-17, 33.6, 1500, {.maxSpeed = 65});
+    chassis.moveToPoint(-26, 22, 1500, {.maxSpeed = 80});
+    intake.move(-127);
+    indexer.move(-127);
+    pros::delay(2000);
+    intake.move(0);
+    indexer.move(0);
+    
+    chassis.moveToPoint(-36,9, 2000, {.maxSpeed = 55});
+    pros::delay(500);
+    scoring.move(-127);
+    indexer.move(-127);
+    intake.move(127);
+    pros::delay(2000);
+    scoring.move(0);
+    indexer.move(0);
+    intake.move(0);
 }
 
 
